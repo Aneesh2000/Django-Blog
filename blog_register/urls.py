890478f47068964452c958ddf1e94713512d4660
Aneesh2000 <aneeshgarray2000@gmail.com>
2021-05-01 20:25:47 +0530

    added some more methods

--- blog_register/urls.py
+++ blog_register/urls.py
@@ -2,6 +2,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.blog_form),
-    path('list/',views.blog_list)
+    path('', views.blog_form,name='blog_insert'),
+    path('<int:id>',views.blog_form,name='blog_update'),
+    path('delete/<int:id>/',views.blog_delete,name='blog_delete'),
+    path('list/',views.blog_list,name='blog_list')
 ]
