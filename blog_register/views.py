25b9a37d9550fd9db8b0151eafa7185439e709c2
Aneesh2000 <aneeshgarray2000@gmail.com>
2021-05-01 15:56:09 +0530

    created templates

--- blog_register/views.py
+++ blog_register/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def blog_list(request):
+    return
+
+def blog_form(request):
+    return
+
+def blog_delete(request):
+    return
